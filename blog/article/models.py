73f6b5850c8c2881e7561650647512d6d8f2e0c0
ukena18 <ukena18@gmail.com>
2022-11-13 23:14:03 -0700

    Update models.py

--- blog/article/models.py
+++ blog/article/models.py
@@ -4,14 +4,14 @@ from ckeditor.fields import RichTextField
 
 # # Create your models here.
 
-# class Article(models.Model):
-#     #point to usr table
-#     author=models.ForeignKey('auth.User',on_delete=models.CASCADE,verbose_name='АВТОР')
-#     title = models.CharField(max_length=50)
-#     content =  RichTextField()
-#     created_date = models.DateTimeField(auto_now_add=True)
-#     article_image = models.FileField(blank=True,null=True,verbose_name='Upload_image')
+class Article(models.Model):
+    #point to usr table
+    author=models.ForeignKey('auth.User',on_delete=models.CASCADE,verbose_name='АВТОР')
+    title = models.CharField(max_length=50)
+    content =  RichTextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    article_image = models.FileField(blank=True,null=True,verbose_name='Upload_image')
 
-#     #in admin ponel chage the name to title
-#     def __str__(self):
-#         return self.title;
+    #in admin ponel chage the name to title
+    def __str__(self):
+        return self.title;
