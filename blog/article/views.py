829f5508a6b0b5cb660036590ad04e16a558fb68
ukena18 <ukena18@gmail.com>
2022-11-18 23:38:42 -0700

    Update views.py

--- blog/article/views.py
+++ blog/article/views.py
@@ -102,9 +102,7 @@ def deleteArticle(request,id):
 def deleteArticle(request,id):
     article = get_object_or_404(Article,id=id)
     article.delete()
-    messages.success(request,'Article deleted')
-    return redirect('article:dashbord')
-
+    messages.success(reque 
 
 
 
