9c2c928b2809be175df7fa7d9a257972c0b1c382
ukena18 <ukena18@gmail.com>
2022-11-18 23:38:54 -0700

    Update views.py

--- blog/article/views.py
+++ blog/article/views.py
@@ -100,9 +100,4 @@ def deleteArticle(request,id):
 
 @login_required(login_url='user:loginUser')
 def deleteArticle(request,id):
-    article = get_object_or_404(Article,id=id)
-    article.delete()
-    messages.succ 
-
-
-
+    article = g
