33a22ffb51327128aef72d45f35f2abc3ff618cf
ukena18 <ukena18@gmail.com>
2022-11-19 23:50:15 -0700

    Update views.py

--- blog/article/views.py
+++ blog/article/views.py
@@ -94,3 +94,28 @@ def deleteArticle(request,id):
     return redirect('article:dashbord')
 
 
+
+      article.save()
+
+        messages.success(request,'Article updatet');
+        return redirect('index')
+    return render(request,'update.html',{'form':form})
+
+@login_required(login_url='user:loginUser')
+def deleteArticle(request,id):
+    article = get_object_or_404(Article,id=id)
+    article.delete()
+    messages.success(request,'Article deleted')
+    return redirect
+          article.save()
+
+        messages.success(request,'Article updatet');
+        return redirect('index')
+    return render(request,'update.html',{'form':form})
+
+@login_required(login_url='user:loginUser')
+def deleteArticle(request,id):
+    article = get_object_or_404(Article,id=id)
+    article.delete()
+    messages.success(request,'Article deleted')
+    return redirect
